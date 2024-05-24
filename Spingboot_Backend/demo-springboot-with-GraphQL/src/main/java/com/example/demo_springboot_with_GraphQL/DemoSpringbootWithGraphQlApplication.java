36691754d0afb7917796d16f5d803857b493810e
oscar301099 <oscarorosduran@gmail.com>
2024-05-24 17:49:52 -0400

    cree proyecto spring boot con todas las dependencias necesarias

---
+++ Spingboot_Backend/demo-springboot-with-GraphQL/src/main/java/com/example/demo_springboot_with_GraphQL/DemoSpringbootWithGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_springboot_with_GraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootWithGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootWithGraphQlApplication.class, args);
+	}
+
+}
---
+++ Spingboot_Backend/demo-springboot-with-GraphQL/src/test/java/com/example/demo_springboot_with_GraphQL/DemoSpringbootWithGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_springboot_with_GraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootWithGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
