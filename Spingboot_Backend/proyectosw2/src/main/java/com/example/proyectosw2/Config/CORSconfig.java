d0b6bd915e7eb79e6faa5feca3afacc29d3f8d50
oscar301099 <oscarorosduran@gmail.com>
2024-06-13 08:56:24 -0400

    ultimo

--- Spingboot_Backend/proyectosw2/src/main/java/com/example/proyectosw2/Config/CORSconfig.java
+++ Spingboot_Backend/proyectosw2/src/main/java/com/example/proyectosw2/Config/CORSconfig.java
@@ -14,7 +14,7 @@ public class CORSconfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- Spingboot_Backend/proyectosw2/src/main/java/com/example/proyectosw2/security/SecurityConfig.java
+++
@@ -1,4 +0,0 @@
-package com.example.proyectosw2.security;
-
-public class SecurityConfig {
-}
