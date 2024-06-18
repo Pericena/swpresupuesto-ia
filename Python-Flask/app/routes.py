ada91f291d630e687083f413728e7555d4cb028d
Luishiño <pericena12@gmail.com>
2024-06-17 23:54:14 -0400

    KPI

--- Python-Flask/app/routes.py
+++ Python-Flask/app/routes.py
@@ -43,7 +43,6 @@ def calcular_gastos(gastos):
     top_conceptos = sorted(categorias.items(), key=lambda x: x[1], reverse=True)[:3]
     return categorias, total_gastos, top_conceptos
 
-
 # Ruta para mostrar el gráfico de gastos
 @main_bp.route('/api/gastos', methods=['GET'])
 def get_gastos_kpi():
@@ -91,7 +90,7 @@ def index():
 @main_bp.route('/distribucion')
 def distribucion():
     # Obtener datos de la API de distribución de gastos por categoría
-    api_url = 'http://127.0.0.1:5000/api/distribucion-gastos-por-categoria'
+    api_url = 'http://152.67.56.238:5000/api/distribucion-gastos-por-categoria'
     try:
         response = requests.get(api_url)
         response.raise_for_status()
@@ -102,7 +101,6 @@ def distribucion():
         # Manejar errores de solicitud a la API
         error_message = f'Error al obtener datos de la API: {str(e)}'
         return render_template('error.html', error_message=error_message)
-
     return render_template('distribucion.html', distribucion_gastos=distribucion_gastos, descripcion=descripcion)
 
 # Función para obtener los gastos desde la API externa
@@ -147,10 +145,8 @@ def obtener_distribucion_gastos_por_categoria():
     gastos = obtener_gastos()
     if 'error' in gastos:
         return jsonify({'error': 'Error al obtener los datos de gastos'})
-    
     distribucion_gastos = calcular_distribucion_gastos(gastos)
     descripcion = generar_descripcion_chatgpt(distribucion_gastos)
-    
     return jsonify({
         'distribucion_gastos': distribucion_gastos,
         'descripcion': descripcion
@@ -162,24 +158,18 @@ def obtener_distribucion_gastos_por_categoria():
 def calcular_top_5_gastos():
     # Obtener la URL de la API de la solicitud
     url = f"{Config.API_URL}/getall"
-    
     # Manejar el caso de error si la URL no está presente en la solicitud
     if not url:
         return {'error': 'API URL missing in request'}, 400
-
     # Obtener los datos de gastos
     gastos = obtener_gastos_top5(url)
-
     # Manejar el caso de error si no se pueden obtener los datos de gastos
     if 'error' in gastos:
         return gastos, 500
-
     # Procesar los datos de gastos
     top_5_categorias = procesar_gastos(gastos)
-
     # Obtener respuesta de GPT-3 para mejorar la descripción
     gpt3_response = obtener_respuesta_gpt3(top_5_categorias)
-
     return {'top_5_categorias': top_5_categorias, 'IA': gpt3_response}
 
 def obtener_gastos_top5(url):
@@ -193,23 +183,18 @@ def obtener_gastos_top5(url):
 def procesar_gastos(gastos):
     # Inicializar un diccionario para almacenar los gastos por categoría y fecha
     gastos_por_categoria_fecha = defaultdict(lambda: defaultdict(float))
-
     # Calcular los gastos totales por categoría y fecha
     for gasto in gastos:
         categoria = gasto['concepto']
         monto = gasto['monto']
         fecha = datetime.strptime(gasto['fecha'], '%Y-%m-%d').date()
-
         gastos_por_categoria_fecha[categoria][fecha] += monto
-
     # Calcular el total de gastos por categoría para cada fecha
     total_gastos_por_categoria_fecha = {}
     for categoria, gastos_por_fecha in gastos_por_categoria_fecha.items():
         total_gastos_por_categoria_fecha[categoria] = sum(gastos_por_fecha.values())
-
     # Obtener las 5 categorías con los gastos más grandes
     top_5_categorias = sorted(total_gastos_por_categoria_fecha.items(), key=lambda x: x[1], reverse=True)[:5]
-
     return top_5_categorias
 
 
@@ -303,7 +288,6 @@ def calcular_promedio_gastos(gastos):
         fecha_anio = fecha.strftime('%Y')
         gastos_por_anio[fecha_anio] += monto
         num_gastos_por_anio[fecha_anio] += 1
-
     promedio_gastos_por_dia = {fecha: gastos_por_dia[fecha] / num_gastos_por_dia[fecha] for fecha in gastos_por_dia}
     promedio_gastos_por_mes = {fecha: gastos_por_mes[fecha] / num_gastos_por_mes[fecha] for fecha in gastos_por_mes}
     promedio_gastos_por_anio = {fecha: gastos_por_anio[fecha] / num_gastos_por_anio[fecha] for fecha in gastos_por_anio}
@@ -319,7 +303,6 @@ def calcular_promedio_gastos(gastos):
     }
 
 # Función para generar la descripción usando OpenAI
-
 def generar_descripcion_promedio(promedio_gastos_por_dia, promedio_gastos_por_mes, promedio_gastos_por_anio):
     # Formatear los promedios de gastos para una mejor presentación
     formatted_promedio_gastos_por_dia = format_promedio_gastos(promedio_gastos_por_dia)
@@ -358,11 +341,74 @@ def obtener_promedio_gastos():
     gastos = obtener_gastos()
     if 'error' in gastos:
         return jsonify({'error': 'Error al obtener los datos de gastos'})
-
     promedio_gastos = calcular_promedio_gastos(gastos)
-
     return jsonify(promedio_gastos)
 
+
+############################################
+# Cargar datos desde el endpoint
+response = requests.get('http://167.99.63.51:27016/api/presupuestosUsuarios')
+data = response.json()
+
+def calculate_kpis(presupuestos):
+    try:
+        # Convertir fechas a objetos datetime
+        for presupuesto in presupuestos:
+            if "fechaInicio" in presupuesto and isinstance(presupuesto["fechaInicio"], str):
+                presupuesto["fechaInicio"] = datetime.strptime(presupuesto["fechaInicio"], "%d-%m-%Y")
+            if "fechaFin" in presupuesto and isinstance(presupuesto["fechaFin"], str):
+                presupuesto["fechaFin"] = datetime.strptime(presupuesto["fechaFin"], "%d-%m-%Y")
+
+        # Si no hay al menos dos presupuestos, no se puede calcular crecimiento ni cambio mensual promedio
+        if len(presupuestos) < 2:
+            return None
+
+        # KPI 1: Crecimiento del Presupuesto
+        crecimiento_presupuesto = ((presupuestos[-1]["montoTotal"] - presupuestos[-2]["montoTotal"]) / presupuestos[-2]["montoTotal"]) * 100
+
+        # KPI 2: Duración del Presupuesto
+        duraciones = [(p["fechaFin"] - p["fechaInicio"]).days for p in presupuestos]
+
+        # KPI 3: Promedio Mensual del Presupuesto
+        promedios_mensuales = []
+        for p, duracion in zip(presupuestos, duraciones):
+            if duracion != 0:
+                promedio_mensual = p["montoTotal"] / (duracion / 30)
+            else:
+                promedio_mensual = 0  # Evitar división por cero
+            promedios_mensuales.append(promedio_mensual)
+
+        # KPI 4: Cambio Mensual Promedio del Presupuesto
+        cambio_mensual_promedio = ((promedios_mensuales[-1] - promedios_mensuales[-2]) / promedios_mensuales[-2]) * 100
+
+        # Resultados
+        kpi_results = {
+            "crecimiento_presupuesto": crecimiento_presupuesto,
+            "duraciones": duraciones,
+            "promedios_mensuales": promedios_mensuales,
+            "cambio_mensual_promedio": cambio_mensual_promedio
+        }
+
+        return kpi_results
+    except Exception as e:
+        print(f"Error calculating presupuesto total: {e}")
+        return None
+
+@main_bp.route('/api/presupuesto-total', methods=['GET'])
+def get_kpis():
+    kpi_results = []
+    for user_data in data:
+        if user_data["presupuestos"]:
+            kpis = calculate_kpis(user_data["presupuestos"])
+            if kpis:
+                kpi_results.append({
+                    "usuario": user_data["usuario"]["nombre"],
+                    "presupuesto total": kpis
+                })
+    return jsonify(kpi_results)
+
+
+######################################
   
 @main_bp.route('/api/example', methods=['GET'])
 def get_example():
